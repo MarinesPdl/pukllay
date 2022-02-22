be87ec26d63640ed7553c57a4a4653720976c049
Emanuel Chareun <emachareun@gmail.com>
2022-02-22 18:24:36 -0300

    el mejor controlador

---
+++ app/controllers/fields_controller.rb
@@ -0,0 +1,39 @@
+class FieldsController < ApplicationController
+  def index
+    @fields = Field.all
+  end
+
+  def show
+    @field = Field.find(params[:id])
+  end
+
+  def new
+    @field = Field.new
+  end
+
+  def create
+    @field = Field.new(field_params)
+    @field.user = current_user
+    if @field.save
+      redirect_to field_path(@field)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @field = Field.find(params[:id])
+  end
+
+   def update
+    @field = Field.find(params[:id])
+    @field.update(field_params)
+    redirect_to fields_path
+  end
+
+  private
+
+  def field_params
+    params.require(:field).permit(:name, :address, :phone_number, :availability)
+  end
+end
