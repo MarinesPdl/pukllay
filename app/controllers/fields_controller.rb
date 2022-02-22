ad191108199923ec28bb264d474142647b21942c
Emanuel Chareun <emachareun@gmail.com>
2022-02-22 16:36:05 -0300

    creacion controller y vistas fields

---
+++ app/controllers/fields_controller.rb
@@ -0,0 +1,38 @@
+class FieldsController < ApplicationController
+  def index
+    @fields = Field.all
+  end
+
+  def show
+    @field = Field.find(params[:id])
+  end
+
+  def new
+    @field = Field.new
+  end
+
+  def create
+    @field = Field.new(field_params)
+    if @field.save
+      redirect_to fields_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @field = Field.find(params[:id])
+  end
+
+   def update
+    @field = Field.find(params[:id])
+    @field.update(field_params)
+    redirect_to fields_path
+  end
+
+  private
+
+  def field_params
+    params.require(:field).permit(:name, :address, :phone_number, :availability)
+  end
+end
